3e4e0f4c4de3dfffb88fcc143ddc3818e2040acf
unitime880 <unitime880@gmail.com>
2024-10-05 17:44:00 +0500

    Update GitTestPython.py

--- GitTestPython.py
+++ GitTestPython.py
@@ -1,5 +1,5 @@
 def most_frequent(list):
     return max(set(list), key = list.count)  
 
-numbers = [1,2,1,2,3,2,1,4,2]
+numbers = [140, 52, 51, 99, 88, 14, 777, 68594059685904, 3456765943045678, 68594034569789605, 68954034567765434]
 most_frequent(numbers)
